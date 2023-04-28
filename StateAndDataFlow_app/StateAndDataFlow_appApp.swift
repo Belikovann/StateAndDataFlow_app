78b00ec7e179529dcce6539f3f6593d465a318b6
@Annabel_ <hydrant94@gmail.com>
2023-04-28 23:36:44 +0300

    Initial Commit

---
+++ StateAndDataFlow_app/StateAndDataFlow_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlow_appApp.swift
+//  StateAndDataFlow_app
+//
+//  Created by Аня Беликова on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlow_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
